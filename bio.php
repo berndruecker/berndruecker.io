5b06752af05afff99c96e185ed6bb319437ca861
Bernd Rücker <bernd.ruecker@camunda.com>
2020-08-20 10:56:35 +0200

    updated bio

--- bio.php
+++ bio.php
@@ -16,11 +16,14 @@
     </div>
       <div class="row">
         <div class="col-md-5">
-          <p><img src='assets/img/en.png' height='20px'></span> I have been the in the software development field for more than 15 years, automating highly scalable workflows at global companies including T-Mobile, Lufthansa and Zalando and contributing to various open source workflow engines. I’m Co-Founder and Chief Technologist of Camunda – an open source software company reinventing workflow automation. Along with my Co-Founder, I wrote "Real-Life BPMN," a popular book about workflow modeling and automation. I regularly speak at international conferences and write for various magazines, focusing on new workflow automation paradigms that fit into modern architectures around distributed systems, microservices, domain-driven design, event-driven architecture and reactive systems.
+          <p><img src='assets/img/en.png' height='20px'></span>I am a software developer at heart who has been innovating process automation deployed in highly scalable and agile environments of industry leaders such as T-Mobile, Lufthansa, ING and Atlassian. I contributed to various open-source workflow engines for more than 15 years and I'm the Co-Founder and Chief Technologist of Camunda – an open-source software company reinventing process automation. I am author of "Practical Process Automation" and co-author of "Real-Life BPMN". Additionally, I am regular speaker at conferences around the world and a frequent contributor to several technology publications. I focus on new process automation paradigms that fit into modern architectures around distributed systems, microservices, domain-driven design, event-driven architecture, and reactive systems.
           </p>
+          <!--
+Bernd Ruecker is a software developer at heart who has been innovating process automation deployed in highly scalable and agile environments of industry leaders such as T-Mobile, Lufthansa, ING and Atlassian. He has contributed to various open-source workflow engines for more than 15 years and is the Co-Founder and Chief Technologist of Camunda – an open-source software company reinventing process automation. Bernd is co-author of the popular book "Real-Life BPMN," now in its sixth edition and available in English, German and Spanish. Additionally, he is a sought-after speaker at conferences around the world and a frequent contributor to several technology publications. Bernd is focused on new process automation paradigms that fit into modern architectures around distributed systems, microservices, domain-driven design, event-driven architecture, and reactive systems.         	
+          -->
         </div>
         <div class="col-md-5">
-          <p><img src='assets/img/de.png' height='20px'></span>Ich entwickle seit über 15 Jahren Software und habe zahlreichen Kunden dabei geholfen, Kernprozesse zu automatisieren, so z.B. der Bestellprozesse bei Zalando, Auftragsprozesse bei T-Mobile oder Patenanträge in der Schweiz. Ich habe aktiv an der Entwicklung verschiedener Open Source Workflow Engines mitgearbeitet und bin Mitgründer der Camunda, ein Open Source Unternehmen das Workflow Automatisierung neu erfindet. Ich bin Co-Autor des "Praxishandbuch BPMN", spreche regelmäßig auf Konferenzen und schreibe für verschiedene Magazine. Seit geraumer Zeit beschäftige ich mit Workflow Automation Paradigmen die in moderne Architekturen rund um Microservices, Domain Driven Design, Event Driven Architecture und reaktiver Systeme passen. </p>
+          <p><img src='assets/img/de.png' height='20px'></span>Ich entwickle seit über 15 Jahren Software und habe zahlreichen Kunden dabei geholfen, Kernprozesse zu automatisieren, so z.B. der Bestellprozesse bei Zalando, Auftragsprozesse bei T-Mobile oder Patenanträge in der Schweiz. Ich habe aktiv an der Entwicklung verschiedener Open Source Workflow Engines mitgearbeitet und bin Mitgründer der Camunda, ein Open Source Unternehmen das Prozessautomatisierung neu erfindet. Ich bin Autor von "Practical Process Automation" und Co-Autor des "Praxishandbuch BPMN", spreche regelmäßig auf Konferenzen und schreibe für verschiedene Magazine. Seit geraumer Zeit beschäftige ich mit Process Automation Paradigmen die in moderne Architekturen rund um Microservices, Domain Driven Design, Event Driven Architecture und reaktiver Systeme passen. </p>
         </div>
         <div class="col-md-2">
           <p>
@@ -38,7 +41,7 @@
     <div style="background: #f5f5f5;">
     <div class="container mt">
         <div class="row clients centered">
-          <p class="mb">The company I co-founded and love to work for as technologist:</p>
+          <p class="mb">The company I co-founded and love to work for as chief technologist:</p>
           <div class="col-sm-12">
             <a href="http://camunda.com"><img src="assets/img/camunda.png" alt=""></a>
           </div>
